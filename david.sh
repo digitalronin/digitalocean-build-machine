51a0f607fec39a66ccfefb23d0e698acd8acddd7
digitalronin <david@digitalronin.com>
2021-02-14 18:42:34 +0900

    Create a script to setup the `david` user
    This still needs work, but it's a start

---
+++ david.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Run this script as david to set up the user account etc.
+
+set -euo pipefail
+
+main() {
+  dotfiles
+  configure_neovim
+}
+
+dotfiles() {
+  cd
+  git clone git@github.com:digitalronin/bin-scripts.git bin
+  git clone git@github.com:digitalronin/dotfiles.git
+
+  cd dotfiles
+  script/bootstrap # TODO: run non-interactively
+}
+
+configure_neovim() {
+  # Neovim
+  mkdir ~/.config
+  cd ~/.config
+  git clone git@github.com:digitalronin/neovim-config.git nvim
+  mkdir -p nvim/tmp/backup nvim/tmp/swap nvim/tmp/undo
+  # TODO: non-interactively install plugins
+}
+
+main
+
