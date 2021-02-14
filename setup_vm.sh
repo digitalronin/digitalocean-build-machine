5990575a000bc290373b3493d18a4efcef96d554
digitalronin <david@digitalronin.com>
2021-02-14 18:42:17 +0900

    Update setup script inc. creating `david` user
    ...and installing software

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-# Add 2G of swap memory
-fallocate -l 2G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+main() {
+  create_users
+  apt-get update
+  make_swap
+  install_software
+}
 
-# install docker
-wget -nv -O - https://get.docker.com/ | sh
+make_swap() {
+  # Add 2G of swap memory
+  fallocate -l 2G /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+}
 
-apt-get update
-apt-get install -y \
-  build-essential \
-  git \
-  ruby2.7
+install_software() {
+  # install docker
+  wget -nv -O - https://get.docker.com/ | sh
 
+  apt-get install -y \
+    build-essential \
+    git \
+    ruby2.7 \
+    tmux \
+    neovim
+}
 
+create_users() {
+  local readonly username=david
+
+  useradd -m -U -s /bin/bash -G sudo ${username}
+  mkdir /home/${username}/.ssh
+  cp /root/.ssh/authorized_keys /home/${username}/.ssh/
+  chown -R ${username}:${username} /home/${username}/.ssh
+}
+
+main
+
+# set password for 'david' user
